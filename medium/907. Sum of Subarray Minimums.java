0362be4008c933d1b04506cabf28be1758ba30f9
Seungyeon Kang <sseungyeon.k@gmail.com>
2022-03-25 22:53:15 +0900

    907. Sum of Subarray Minimums.java

--- medium/907. Sum of Subarray Minimums.java
+++ medium/907. Sum of Subarray Minimums.java
@@ -1,33 +1,45 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
+        //TC : O(n);
+        //SC : O(n)
+        int mod = (int)1e9 + 7;
+        int len = arr.length;
+        Stack<int[]> stack = new Stack<>();
+        long sum = 0;
+        int[] left = new int[len];
+        int[] right = new int[len];
         
-        long res = 0, mod = (int)1e9 + 7;
-        int n = arr.length;
-        int[] left = new int[n], right = new int[n];
-        Stack<int[]> s1 = new Stack<>(), s2 = new Stack<>();
-        
-        for(int i = 0; i < n; i++){
-            int count = 1;
-            while(!s1.isEmpty() && s1.peek()[0] > arr[i]) {
-                count += s1.pop()[1];
+        for(int i=0;i<len;i++){
+            if(stack.isEmpty() || stack.peek()[0]< arr[i])  {
+                stack.push(new int[]{arr[i],1});
+                left[i] = 1;
             }
-            s1.push(new int[]{arr[i], count});
-            left[i] = count;
+            else {
+                int[] tmp = stack.pop();
+                stack.push(new int[]{arr[i], tmp[1]+1});
+                left[i] = tmp[1]+1;
+            }
         }
         
-        for(int j = n-1; j >=0; j--) {
-            int count = 1;
-            while(!s2.isEmpty() && s2.peek()[0] >= arr[j]) {
-                count+=s2.pop()[1];
+        while(!stack.isEmpty()) {
+            stack.pop();
+        }
+        
+        for(int i = len-1; i>=0; i--) {
+            if(stack.isEmpty() || stack.peek()[0] < arr[i]) {
+                stack.push(new int[]{arr[i],1});
+                right[i] = 1;
+            } else {
+               int[] tmp = stack.pop();
+                stack.push(new int[]{arr[i], tmp[1]+1});
+                right[i] = tmp[1]+1; 
             }
-            s2.push(new int[]{arr[j], count});
-            right[j] = count;
         }
-            
-        for(int i=0; i<n; i++) {
-            res=(res + (long)arr[i]*left[i]*right[i]) % mod;
+        
+        for(int i=0;i<len;i++){
+            sum = sum + (long)(left[i]* right[i]*arr[i])%mod;
         }
-        return (int) res;
-            
+        
+        return (int)sum;
     }
 }
